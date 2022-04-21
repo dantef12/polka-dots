5a127f94b2bbc6ceb37507b1aab311a65a7a57a7
dantef12 <100299384+dantef12@users.noreply.github.com>
2022-04-21 10:46:29 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index <= 5; index++) {
+    for (let index = 0; index <= 4; index++) {
         led.plot(index + 1, index)
-        led.plot(0 + 0, 0)
+        led.plot(index + 3, index)
+        led.plot(index, index + 1)
+        led.plot(index, index + 3)
+        basic.pause(300)
     }
 })
 basic.forever(function () {
